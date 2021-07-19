b51d6d1cc1f18db64e518bc056df00ca4d1af320
Marek Cottingham <TheMuonNeutrino@users.noreply.github.com>
2021-07-19 14:25:58 +0100

    .

--- Example Code/processingExample.py
+++ Example Code/processingExample.py
@@ -12,7 +12,7 @@ event2008_12_07 = (datetime(2008,12,7), datetime(2008,12,10))
 event2011_09_02 = (datetime(2011, 9, 2), datetime(2011, 9, 6))
 
 mag.importCdasAsync(
-    *event2011_09_02
+    *event2008_12_07
 )
 mag.magneticField.removeDuplicateTimes()
 mag.peemIdentifyMagnetosheath.removeDuplicateTimes()
@@ -41,17 +41,19 @@ tor.phaseVocoderStretch(16)
 tor.normalise()
 tor.genMonoAudio("Example of tor x16 with phase vocoder.wav")
 
-com = mag.magneticFieldMeanFieldCoordinates.extractKey(0)
-com.waveletStretch(16)
-com.normalise()
-com.genMonoAudio("Example of com x16 with wavelets.wav")
+# Only run these if you have time for the wavelet stretch to run
 
-pol = mag.magneticFieldMeanFieldCoordinates.extractKey(1)
-pol.waveletStretch(16)
-pol.normalise()
-pol.genMonoAudio("Example of pol x16 with wavelets.wav")
+# com = mag.magneticFieldMeanFieldCoordinates.extractKey(0)
+# com.waveletStretch(16)
+# com.normalise()
+# com.genMonoAudio("Example of com x16 with wavelets.wav")
 
-tor = mag.magneticFieldMeanFieldCoordinates.extractKey(2)
-tor.waveletStretch(16)
-tor.normalise()
-tor.genMonoAudio("Example of tor x16 with wavelets.wav")
+# pol = mag.magneticFieldMeanFieldCoordinates.extractKey(1)
+# pol.waveletStretch(16)
+# pol.normalise()
+# pol.genMonoAudio("Example of pol x16 with wavelets.wav")
+
+# tor = mag.magneticFieldMeanFieldCoordinates.extractKey(2)
+# tor.waveletStretch(16)
+# tor.normalise()
+# tor.genMonoAudio("Example of tor x16 with wavelets.wav")
