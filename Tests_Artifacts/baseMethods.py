71bc0e584e2840c485e414f49bd324517848ba19
Marek Cottingham <TheMuonNeutrino@users.noreply.github.com>
2021-07-20 09:33:06 +0100

    Documented baseMethods

--- Tests_Artifacts/baseMethods.py
+++ Tests_Artifacts/baseMethods.py
@@ -20,6 +20,7 @@ from scipy.optimize.minpack import curve_fit
 
 
 def getBeforeAndExpectation_Sine(freq,duration_seconds,stretch = 16):
+    """Get generated data sets for a simulated sine wave, before and after a time stretch"""
     ts = _getTestingTimeSeries(duration_seconds)
     genFunc = SimulateData().genSine
     genFuncExpectaion = SimulateData().genSineExpectation
@@ -32,6 +33,7 @@ def getBeforeAndExpectation_Sine(freq,duration_seconds,stretch = 16):
     return before, expectation
 
 def getBeforeAndExpectation_Harmonic(freqs,duration_seconds,stretch = 16):
+    """Get generated data sets for a simulated harmonic, before and after a time stretch"""
     ts = _getTestingTimeSeries(duration_seconds)
     genFunc = SimulateData().genHarmonic
     genFuncExpectaion = SimulateData().genHarmonicExpectation
@@ -43,13 +45,16 @@ def getBeforeAndExpectation_Harmonic(freqs,duration_seconds,stretch = 16):
     )
     return before, expectation
 
-def getBeforeAndExpectation_Sweep(f0,f1,duration_seconds,stretch=16,method='logarithmic'):
+def getBeforeAndExpectation_Sweep(
+    startingFreq,finalFreq,duration_seconds,stretch=16,method='logarithmic'
+):
+    """Get generated data sets for a simulated frequency sweep, before and after a time stretch"""
     ts = _getTestingTimeSeries(duration_seconds)
     genFunc = SimulateData().genSweep
     genFuncExpectation = SimulateData().genSweepExpectation
     kwargs = {
-        'f0': f0,
-        'f1': f1,
+        'f0': startingFreq,
+        'f1': finalFreq,
         'method': method
     }
     before, expect = _getBeforeAndExpectation_Func(
@@ -88,16 +93,20 @@ def _getTestingTimeSeries(duration_seconds):
     
     return ts
 
-def PSD(dataSet: DataSet_1D):
+def PSD(dataSet: DataSet_1D) -> np.array:
+    """Compute the PSD of a data set"""
     return np.abs(np.fft.rfft(dataSet.x))**2 / len(dataSet.x)
 
-def PSD_freqs(dataSet: DataSet_1D,sampleRate=44100):
+def PSD_freqs(dataSet: DataSet_1D,sampleRate=44100) -> np.array:
+    """Compute the corresponding frequencies for the PSD of a data set"""
     return np.fft.rfftfreq(len(dataSet.x),d=1/sampleRate)
 
 def normalisePSD(psd):
+    """Normalise a PSD so that its highest peak has a magnitude of 1"""
     return psd/np.max(psd)
 
 def plotPSD(expectation,after,showPlot=True):
+    """Plots the PSD of the provided pair of data sets"""
     expectationPSD = PSD(expectation)
     actualPSD = PSD(after)
     expectation_frequencies = PSD_freqs(expectation)
@@ -115,34 +124,51 @@ def plotPSD(expectation,after,showPlot=True):
         plt.show()
 
 def reducePlotResolution(frequencies,PSD,n=1000):
+    """Reduce PSD resolution to allow plotting with matplotlib. Smooths the PSD.
+
+    :returns: ``( reducedFreq: np.array, reducedPSD: np.array )``
+    """
     reducedFreq = np.linspace(min(frequencies),max(frequencies),n)
     smoothPSD = uniform_filter1d(
         PSD,len(PSD)//n,mode='constant',cval=0, origin=0
     )
     f = interp1d(frequencies,smoothPSD,'linear')
-    redPSD = f(reducedFreq)
-    redPSD = normalisePSD(redPSD)
-    return reducedFreq, redPSD
+    reducedPSD = f(reducedFreq)
+    reducedPSD = normalisePSD(reducedPSD)
+    return reducedFreq, reducedPSD
 
 def getAfterAlgorithm(before: magSonify.DataSet_1D,stretch,algorithm):
+    """Process data series with the given time stretch algorithm
+    
+    :param algorithm:
+        Can be 'waveletStretch', 'paulStretch', 'phaseVocoderStretch' or 'wsolaStretch'
+    """
     after = before.copy()
     getattr(after,algorithm)(stretch)
     return after
 
 def plotPSD_Sine(algorithm,freq, stretch, showPlot=True):
+    """See also: :func:`baseMethods.compare_Sine`"""
     expectation, after = compare_Sine(algorithm, freq, stretch)
     plotPSD(expectation,after,showPlot)
 
 def compare_Sine(algorithm, freq, stretch):
+    """Computes the expeted and actual time stretch on a sine wave with the given algorithm
+    
+    See also: :func:`baseMethods.getAfterAlgorithm`"""
     before, expectation = getBeforeAndExpectation_Sine(freq,0.2,stretch)
     after = getAfterAlgorithm(before, stretch,algorithm)
     return expectation,after
 
 def plotPSD_Harmonic(algorithm,freqs, stretch, showPlot=True):
+    """See also: :func:`baseMethods.compare_Harmonic`"""
     expectation, after = compare_Harmonic(algorithm, freqs, stretch)
     plotPSD(expectation,after,showPlot)
 
 def compare_Harmonic(algorithm, freqs, stretch):
+    """Computes the expeted and actual time stretch on a harmonic with the given algorithm
+    
+    See also: :func:`baseMethods.getAfterAlgorithm`"""
     before,expectation = getBeforeAndExpectation_Harmonic(freqs,0.2,stretch)
     after = getAfterAlgorithm(before,stretch,algorithm)
     return expectation, after
