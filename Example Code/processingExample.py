02cdacd00b4cb7e38dfff0aceb50d35bd9d3ba24
Marek Cottingham <TheMuonNeutrino@users.noreply.github.com>
2021-07-20 07:33:48 +0100

    Added explanatory comments

--- Example Code/processingExample.py
+++ Example Code/processingExample.py
@@ -11,21 +11,36 @@ event2007_09_04 = (datetime(2007,9,4), datetime(2007,9,5))
 event2008_12_07 = (datetime(2008,12,7), datetime(2008,12,10))
 event2011_09_02 = (datetime(2011, 9, 2), datetime(2011, 9, 6))
 
+# Import the satellite data, specifying which themis satellite to use
 mag.importCdasAsync(
-    *event2008_12_07
+    *event2008_12_07,
+    satellite='D'
 )
+
+# Sometimes CDAS has duplicate time entries, we remove all but the first in order to prevent
+# errors when interpolating the data
 mag.magneticField.removeDuplicateTimes()
 mag.peemIdentifyMagnetosheath.removeDuplicateTimes()
+
+# Interpolate the data to a consistent, know spacing
 mag.interpolate_3s()
+
 mag.magneticField.constrainAbsoluteValue(400)
+
+# Compute the mean field, this is both to allow the mean field to be subtracted and for use later
+# in .convertToMeanFieldCoordinates(). Note that the exact attribute name: .meanField must be 
+# preserved in order for .convertToMeanFieldCoordinates() to work.
 mag.meanField = mag.magneticField.runningAverage(timeWindow=np.timedelta64(35,"m"))
 mag.magneticField = mag.magneticField - mag.meanField
+
 mag.fillLessThanRadius(4)
 mag.removeMagnetosheath()
 mag.convertToMeanFieldCoordinates()
 
 mag.magneticFieldMeanFieldCoordinates.fillNaN()
 
+# Extract each of the field components as a 1D data set, perform the time stretch and output as
+# audio
 com = mag.magneticFieldMeanFieldCoordinates.extractKey(0)
 com.phaseVocoderStretch(16)
 com.normalise()
