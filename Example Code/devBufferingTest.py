236d29ca92ced60441d468836e2ba63860a02ee9
Marek Cottingham <TheMuonNeutrino@users.noreply.github.com>
2021-07-22 19:04:38 +0100

    development experiments with buffering

---
+++ Example Code/devBufferingTest.py
@@ -0,0 +1,38 @@
+import context
+context.get()
+
+from multiprocessing import Manager, Queue
+import magSonify
+from datetime import datetime ,timedelta
+from magSonify import THEMISdata
+from magSonify.Buffering import BaseProcess
+
+dataClass = THEMISdata
+start = datetime(2007,9,21)
+intervalHours = 12
+events = ((
+    start+timedelta(hours=(i)*intervalHours),
+    start+timedelta(hours=(i+1)*intervalHours)
+    ) for i in range(20))
+events = list(events)
+
+if __name__ == "__main__":
+    manager = Manager()
+    queues = (manager.Queue(maxsize=5) for i in range(3))
+    queues = list(queues)
+    processes = {}
+    for task, taskArgs in {
+        "importer": (events,), 
+        "processing": (), 
+        "sonification": (1,"waveletStretch",(12,0.5,12)), 
+        "playback": (44100//2,)
+    }.items():
+        processes[task] = BaseProcess(
+            task=task,
+            taskArgs=taskArgs,
+            queues=queues
+        )
+        processes[task].start()
+    for task, p in processes.items():
+        p.join()
+        print(p,"joined")
---
+++ magSonify/Buffering.py
@@ -0,0 +1,144 @@
+
+from threading import local
+
+from numpy.lib.shape_base import apply_along_axis
+from magSonify.DataSet_1D import DataSet_1D
+from magSonify.MagnetometerData import MagnetometerData, THEMISdata
+import multiprocessing as mp
+import sounddevice
+from timeit import default_timer as timer
+import sys, os
+import numpy as np
+from time import sleep
+
+class STOPVALUE:
+    pass
+
+class HiddenPrints:
+    def __enter__(self):
+        self._original_stdout = sys.stdout
+        sys.stdout = open(os.devnull, 'w')
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        sys.stdout.close()
+        sys.stdout = self._original_stdout
+
+class BaseProcess(mp.Process):
+
+    def __init__(self, task, taskArgs, queues, name=None):
+        if name is None:
+            name = task
+        super().__init__(
+            target = getattr(self,task),
+            args = taskArgs,
+            name = name,
+        )
+        self.importedQueue = queues[0]
+        self.processedQueue = queues[1]
+        self.sonifiedQueue = queues[2]
+        self.startTime = timer()
+
+    def importer(self, events: tuple, dataClass = THEMISdata):
+        """
+        :param dataClass:
+            The high level class for satellite data, subclassed from 
+            :MagnetometerData. eg. THEMISdata
+            Should implement ``importCDAS(startDatetime,endDateTime,[*args])``
+            Should implement ``defaultProcessing()``
+        :param events: 
+            Tuple of Tuple entry, each entry contain args for importCDAS in the form
+            (startDatetime,endDatetime,*args)
+        """
+
+        for event in events:
+            try:
+                mag: THEMISdata = dataClass()
+                with HiddenPrints():
+                    mag.importCDAS(*event)
+                self.importedQueue.put(mag)
+            except Exception as e:
+                print(f"Exception for event starting on {event[0]}, skipping")
+                print(e)
+                pass
+            #print(f"Imported {event[0]} @ {timer() - self.startTime} s")
+        self.importedQueue.put(STOPVALUE())
+
+    def processing(self, processingArgs: tuple = ()):
+        try:
+            while True:
+                mag: THEMISdata = self.importedQueue.get()
+                if isinstance(mag, STOPVALUE):
+                    break
+                mag.defaultProcessing(*processingArgs)
+                self.processedQueue.put(mag)
+                #print(f"Processed {mag.magneticField.timeSeries.getStart()} @ {timer() - self.startTime} s")
+            self.processedQueue.put(STOPVALUE())
+        except Exception:
+            self.processedQueue.put(STOPVALUE())
+            print("Exception on interval starting at:",mag.position.timeSeries.getStart())
+            raise
+
+    def sonification(self,axis: int = 1, algorithm: str = "waveletStretch", algArgs: tuple = (16, 0.5, 16)):
+        while True:
+            mag: THEMISdata = self.processedQueue.get()
+            if isinstance(mag, STOPVALUE):
+                break
+            ax = mag.magneticFieldMeanFieldCoordinates.extractKey(axis)
+            getattr(ax,algorithm)(*algArgs)
+            ax.normalise()
+            self.sonifiedQueue.put(ax)
+            #print(f"Sonified {mag.magneticField.timeSeries.getStart()} @ {timer() - self.startTime} s")
+        self.sonifiedQueue.put(STOPVALUE())
+
+    def playback(self,sampleRate=44100//2):
+
+        availableAudio = np.zeros(1,dtype=np.float64)
+        audioLock = mp.Lock()
+        finishAudioProcessingEvent = mp.Event()
+        donePlayingEvent = mp.Event()
+        i = 0
+
+        def callback(outdata,frames,time,status):
+            nonlocal availableAudio
+            nonlocal i
+            if status:
+                print(status)
+            with audioLock:
+                if len(availableAudio) < frames:
+                    outdata[:len(availableAudio),0] = availableAudio[:]
+                    outdata[len(availableAudio):].fill(0)
+                    availableAudio = np.zeros(2,dtype=np.float64)
+                    if finishAudioProcessingEvent.is_set():
+                        raise sounddevice.CallbackStop
+                    print("    Waiting on audio...")
+                else:
+                    outdata[:,0] = availableAudio[0:frames]
+                    availableAudio = availableAudio[frames:]
+                print("Audio left: ", round(len(availableAudio)/44100,1),"s       ",end='\r',flush=True)
+            i += 1
+
+        myStream = sounddevice.OutputStream(
+            samplerate=sampleRate//2,
+            blocksize=44100//10,
+            callback=callback,
+            finished_callback=donePlayingEvent.set,
+            channels = 1,
+        )
+
+        while True:
+            if len(availableAudio) > 44100 * 30:
+                sleep(0.01)
+                continue
+            ax: DataSet_1D = self.sonifiedQueue.get()
+            if isinstance(ax, STOPVALUE):
+                finishAudioProcessingEvent.set()
+                break
+            #print(f"Playing {ax.timeSeries.getStart()} @ {timer() - self.startTime})")
+            
+            with audioLock:
+                availableAudio = np.concatenate((availableAudio,ax.x))
+
+            if myStream.stopped:
+                myStream.start()
+        donePlayingEvent.wait()
+            
