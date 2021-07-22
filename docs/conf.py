45322caffcf5a4c86ad22a174d3ca2be64c459e9
Marek Cottingham <TheMuonNeutrino@users.noreply.github.com>
2021-07-22 18:21:37 +0100

    Added sounddevice mock

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return MagicMock()
 
-MOCK_MODULES = ['soundfile','matplotlib','matplotlib.pyplot']
+MOCK_MODULES = ['soundfile','matplotlib','matplotlib.pyplot','sounddevice']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # -- Project information -----------------------------------------------------
