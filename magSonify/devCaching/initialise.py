7f1d792b8c5f47ae72ae34b0d0f1f311b157527b
Marek Cottingham <TheMuonNeutrino@users.noreply.github.com>
2021-07-23 11:22:00 +0100

    devCaching/initalise import failed fix

--- magSonify/devCaching/initialise.py
+++ magSonify/devCaching/initialise.py
@@ -1,17 +1,21 @@
 import os
 
-local_app_path = os.path.join(
-    os.getenv("LOCALAPPDATA"),
-    "magSonify"
-)
+localappdata = os.getenv("LOCALAPPDATA"),
 
-if local_app_path is not None:
+
+
+if localappdata is not None:
+    local_app_path = os.path.join(
+        localappdata,
+        "magSonify"
+    )
     cdas_cache_path = os.path.join(
         local_app_path,
         "CDAScache"
     )
 else:
     print("Failed to get cache path, do not use cache operations")
+    local_app_path = None
     cdas_cache_path = None
 
 def ensurePath(directory):
