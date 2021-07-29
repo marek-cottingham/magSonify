eb4c61939d5379d9da4e0fcbb06e17c342fb759c
Marek Cottingham <TheMuonNeutrino@users.noreply.github.com>
2021-07-29 07:46:36 +0200

    Updated paulstretch diagram generation

--- Example Code/Sonification Algorithm Diagrams/generatePaulstretchDiagram.py
+++ Example Code/Sonification Algorithm Diagrams/generatePaulstretchDiagram.py
@@ -1,3 +1,4 @@
+
 import core
 import context
 context.get()
@@ -34,7 +35,7 @@ showFullDay_spetrogram = False
 pol, polBefore = core.setup(event)
 
 pol._stretchTimeseries(stretch)
-pol.x, coefficients = paulstretch(pol.x,stretch,0.015,debugOutput=True)
+pol.x, coefficients, startPos, windowSeries = paulstretch(pol.x,stretch,0.015,debugOutput=True)
 pol._correctTimeseries()
 
 if useSubplotVariedLengths:
@@ -45,11 +46,11 @@ if useSubplotVariedLengths:
     ax3: matplotlib.axes.Axes = plt.subplot2grid((3, 16), (2, 0), rowspan=1, colspan=16)
 else:
     fig, (ax1, ax2, ax3) = plt.subplots(3,1,figsize=(8,4.5),sharex='all')
+    ax1: matplotlib.axes.Axes = ax1
+    ax2: matplotlib.axes.Axes = ax2
+    ax3: matplotlib.axes.Axes = ax3
 
-
-ax1: matplotlib.axes.Axes = ax1
-ax2: matplotlib.axes.Axes = ax2
-ax3: matplotlib.axes.Axes = ax3
+ax1r = ax1.twinx()
 
 xlim1 = slice(xlow,xhigh)
 xlim2 = slice(xlow*stretch,xhigh*stretch)
@@ -58,12 +59,12 @@ timesBefore = polBefore.timeSeries.asFloat()
 times = pol.timeSeries.asFloat()
 
 N = coefficients.shape[0]/len(polBefore.x)
-xlimcoeffs = slice(int(xlow*N),int(xhigh*N)+2)
+xlimcoeffs = slice(int(xlow*N)-1,int(xhigh*N)+2)
 
 # Disable the use of range restriction
-if showFullDay_spetrogram: 
+if showFullDay_waveforms: 
     xlim1 = xlim2 = slice(None,None)
-if showFullDay_waveforms:
+if showFullDay_spetrogram:
     xlimcoeffs = slice(None,None)
 
 
@@ -73,24 +74,38 @@ preStretchX = timesBefore[xlim1] - timesBefore[xlow]
 
 timesRelativeToIntervalStart = timesBefore - timesBefore[xlow]
 
-ax1.plot(preStretchX,polBefore.x[xlim1])
+for i,pos in enumerate(startPos[xlimcoeffs]):
+    
+    if i < 4:
+        color = "C1"
+        
+        l2, = ax1r.plot(
+            timesRelativeToIntervalStart[
+                pos:pos+len(windowSeries)
+            ],
+            windowSeries,
+            color=color,
+        )
 
-coefficientsTimes = np.linspace(
-    timesRelativeToIntervalStart[0],
-    timesRelativeToIntervalStart[-1],
-    coefficients.shape[0]+1
-)[:-1]
-coefficientsTimes += (timesRelativeToIntervalStart[1]-timesRelativeToIntervalStart[0])/2
+l1, = ax1.plot(preStretchX,polBefore.x[xlim1])
+
+# coefficientsTimes = np.linspace(
+#     timesRelativeToIntervalStart[0],
+#     timesRelativeToIntervalStart[-1],
+#     coefficients.shape[0]+1
+# )[:-1]
+coefficientsTimes = timesRelativeToIntervalStart[startPos]
+coefficientsTimes = coefficientsTimes + (timesRelativeToIntervalStart[1]-timesRelativeToIntervalStart[0])/2
 
 coefficients = coefficients[xlimcoeffs,:]
 coefficients = np.log(coefficients)
 coefficientsTimes = coefficientsTimes[xlimcoeffs]
 
-print(coefficientsTimes)
+freqs = np.fft.rfftfreq(len(windowSeries),3)
 
 ax2.pcolormesh(
     coefficientsTimes,
-    np.arange(0,coefficients.shape[1]),
+    freqs,
     coefficients.T,
     shading='auto',
     cmap=cmap,
@@ -109,17 +124,30 @@ postStretchX = pol.timeSeries.asFloat()[xlim2] - pol.timeSeries.asFloat()[xlow*s
 ax3.plot(postStretchX,pol.x[xlim2])
 
 ax2.set_yscale('log')
+ax2.set_ylim([
+    freqs[1],freqs[-50]
+])
 
 ax3.set_xlabel("Time since start of interval [s]")
 ax1.set_ylabel("Field [nT]")
 ax2.set_ylabel("Frequency [Hz]")
 ax3.set_ylabel("Amplitude")
+ax1r.set_ylabel("Window amplitude")
+
+ax1.set_zorder(ax1r.get_zorder()+1)
+ax1.patch.set_visible(False)
+ax1.yaxis.label.set_color(l1.get_color())
+ax1.tick_params(axis='y', colors=l1.get_color())
+ax1r.yaxis.label.set_color(l2.get_color())
+ax1r.tick_params(axis='y', colors=l2.get_color())
 
 for ax in (ax1, ax2):
     plt.setp(ax.get_xticklabels(), visible=False)
 
 for ax in (ax1,ax2,ax3):
-    ax.set_xlim([preStretchX[0],preStretchX[-1]])
+    if not showFullDay_waveforms and not showFullDay_spetrogram:
+        ax.set_xlim([preStretchX[0],preStretchX[-1]])
 
-plt.show()
+plt.tight_layout()
 plt.savefig("Paulstretch Diagram.svg")
+#plt.show()
